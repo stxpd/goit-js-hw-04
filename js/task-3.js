e700d8e35161e48edd0f77a30efdbb95d15177bd
stpd <planshetsams12382@gmail.com>
2025-07-28 19:19:09 +0200

    s

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,24 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
-    }
-  }
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  return filteredNumbers;
-}
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
